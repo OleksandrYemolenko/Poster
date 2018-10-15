3a138de4d3e3f4b8af52f23003c7f04e10e8d4fd
Sasha <saaho.rmolenko@gmail.com>
2018-10-15 09:56:31 +0300

    Some design hernya

--- app/src/main/java/com/codersinlow/poster/DishItem.java
+++ app/src/main/java/com/codersinlow/poster/DishItem.java
@@ -37,7 +37,7 @@ public class DishItem extends AppCompatActivity {
         // Передать в dishList список блюд с именем (title) и картиткой (img)
         dishList.add(new DishItem("Hot Dog", "https://www.micccp.com/wp/wp-content/uploads/2016/02/4111.jpg", "30.00₴"));
         dishList.add(new DishItem("Coffee", "https://million-wallpapers.ru/wallpapers/2/56/294040267495523.jpg", "25.00₴"));
-        dishList.add(new DishItem("Juice", "http://www.kartinkijane.ru/pic/201304/1680x1050/kartinkijane.ru-7999.jpg", "28.00₴"));
+        dishList.add(new DishItem("Juice", "http://images.media-allrecipes.com/userphotos/960x960/3758394.jpg", "28.00₴"));
         dishList.add(new DishItem("Pizza", "http://food.studiofact.ru/upload/iblock/706/7062914c3c69c543991a45ca006e456b.jpg", "300.00₴"));
         dishList.add(new DishItem("Cake", "https://31p86334w2bvkz0249eyr0cr-wpengine.netdna-ssl.com/wp-content/uploads/2013/04/triple-chocolate-cake-4-600x900.jpg", "150.00₴"));
 
