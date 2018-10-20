d39bf795f60fe839ceb2e89272b67348d1356fa8
Oleg Fomenko <oleg.fomenko2002@yandex.ru>
2018-10-20 13:05:45 +0300

    add handler

---
+++ app/src/main/java/com/codersinlow/poster/Handler.java
@@ -0,0 +1,40 @@
+package com.codersinlow.poster;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Handler {
+    public static final String token = "0024613c1444f731bb96dd97f025d41a";
+    public static final String format = "json";
+    public static String s = "https://oleg-fomenko.joinposter.com/api/";
+
+    public static String sendRequest(String request, String requestType, String... parms) {
+        String ss = s + request + "?token=" + token + "&format=" + format;
+        for(String p : parms) ss += "&" + p;
+
+        try {
+            URL url = new URL(ss);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod(requestType);
+            BufferedReader bf = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            String content = "", line = "";
+            while((line = bf.readLine()) != null) {
+                content += line;
+            }
+
+            return content;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "error";
+    }
+}
+
